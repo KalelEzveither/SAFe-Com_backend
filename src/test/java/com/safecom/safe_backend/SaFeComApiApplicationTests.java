29af58840f7f126138c608a9c34defeb91350546
Fernando Nardes <frnnardes@gmail.com>
2025-11-18 09:50:52 -0300

    refactor: Refatorando o nome do projeto

---
+++ src/test/java/com/safecom/safe_backend/SaFeComApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safecom.safe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaFeComApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
